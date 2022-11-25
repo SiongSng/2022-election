a62caad2785adf0d706d802a83909897fc87fbc3
菘菘 <rrt467778@gmail.com>
2022-11-25 22:16:28 +0800

    feat: crawler council and referendum data

---
+++ src/script/crawlers/referendum.ts
@@ -0,0 +1,65 @@
+import { writeJson } from '@/script/util';
+import fetch from 'cross-fetch';
+
+/**
+ * 爬取憲法修正案公民複決的資料
+ * 資料來源: 中華民國中央選舉委員會/財團法人中央通訊社
+ */
+export async function crawlerReferendumData() {
+  const timestamp = new Date().getTime();
+  const url = `https://www.cna.com.tw/project/20221126-election-live/api/result.json?t=${timestamp}`;
+
+  const response = await fetch(url);
+
+  if (response.status === 200) {
+    const rawData = await response.json();
+
+    const result: ReferendumData = {
+      processRage: rawData.processRage,
+      agreeTickets: rawData.agreeTicket,
+      agreeRate: rawData.agreeRat,
+      disagreeTickets: rawData.disagreeTicket,
+      disagreeRate: rawData.disagreeRat,
+      timestamp: Date.parse(rawData.time),
+    };
+
+    writeJson('referendum', result);
+  } else {
+    throw new Error('Failed to crawler referendum data.');
+  }
+}
+
+/**
+ * 憲法修正案公民複決的資料格式
+ */
+export interface ReferendumData {
+  /**
+   * 開票進度
+   */
+  processRage: number;
+
+  /**
+   * 同意票數量
+   */
+  agreeTickets: number;
+
+  /**
+   * 同意票比例
+   */
+  agreeRate: number;
+
+  /**
+   * 不同意票數量
+   */
+  disagreeTickets: number;
+
+  /**
+   * 不同意票比例
+   */
+  disagreeRate: number;
+
+  /**
+   * 資料更新時間 (Unix Timestamp)
+   */
+  timestamp: number;
+}
---
+++ src/script/index.ts
@@ -0,0 +1,58 @@
+import { exit } from 'node:process';
+import { pushChangesToGithub } from '@/script/util';
+import { crawlerReferendumData } from '@/script/crawlers/referendum';
+import { crawlerCouncilData } from '@/script/crawlers/council';
+
+const githubAPIToken = process.env.GITHUB_API_TOKEN;
+
+// Check if the script is running on github action
+const githubAction =
+  githubAPIToken != undefined && githubAPIToken != '' && githubAPIToken != null;
+
+async function start() {
+  let runs = 0;
+  let second = 0;
+  await _start();
+
+  async function _start() {
+    await startCrawler();
+    try {
+      if (githubAction) pushChangesToGithub(runs, githubAPIToken);
+    } catch (error) {
+      console.error('Failed to push changes to github.', error);
+    }
+    console.log('Finished');
+  }
+
+  if (githubAction) {
+    setInterval(() => {
+      // Exit the process after three hours
+      if (second >= 60 * 60 * 3) {
+        console.log('Exiting...');
+        exit(0);
+      }
+      second++;
+    }, 1000);
+
+    // Runs every minute
+    setInterval(() => {
+      runs++;
+
+      _start().then(() => console.log(`[${runs}] Waiting for next crawler...`));
+    }, 1000 * 60);
+  }
+}
+
+async function startCrawler() {
+  console.log('Crawling referendum data...');
+  crawlerReferendumData();
+  console.log('Finished crawling referendum data.');
+
+  console.log('Crawling council data...');
+  crawlerCouncilData();
+  console.log('Finished crawling council data.');
+
+  console.log('Crawler the data successfully');
+}
+
+start();
